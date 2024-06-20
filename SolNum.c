f8557ac210d6b9d8036bae6c06dfe704f50eb895
Pedro-Soto <sotodelrosariop@gmail.com>
2024-06-20 16:41:40 -0600

    Added comment

--- SolNum.c
+++ SolNum.c
@@ -1,3 +1,24 @@
+/**
+ * Calculates the pressure gradient in a canal given the pressure at the start and end of the canal, 
+ * the length of the canal, and a time step delta_t.
+ * 
+ * This program prompts the user to input the values for delta_t, L, P1, and P2, and then calculates 
+ * the pressure gradient as the difference in pressure divided by the length of the canal.
+ * 
+ * Example usage:
+ * 
+ * Enter value for Delta_t:
+ * 1
+ * Enter value for L:
+ * 10
+ * Enter value for pressure at canal start |P1|:
+ * 100.0
+ * Enter value for pressure at canal end: |P2|
+ * 200.0
+ * deltaP = 100.000000
+ * 
+ * @return 0 on successful execution, 1 on error
+ */
 #include <stdio.h>
 #include <math.h>
 
@@ -5,14 +26,14 @@ int main () {
     // Declare all variables
     int delta_t;
     printf("Enter value for Delta_t:\n");
-    if (scanf("%d" ,&delta_t)!=1) {
+    if (scanf("%d",&delta_t)!=1) {
         fprintf(stderr,"Error: Delta_t only accepts integer value\n");    
         return 1;    
     }
     int t;
     int L;
     printf("Enter value for L:\n");
-    if (scanf("%d" ,&L)!=1) {
+    if (scanf("%d",&L)!=1) {
         fprintf(stderr,"Error: L only accepts integer value\n");
         return 1; 
     }
@@ -22,14 +43,14 @@ int main () {
     //starting pressure
     float P1;
     printf("Enter value for pressure at canal start |P1|:\n");
-    if (scanf("%f" ,&P1)!=1) {
+    if (scanf("%f",&P1)!=1) {
         fprintf(stderr,"Error: P1 only accepts float value\n");
         return 1;
     }
     //ending pressure, should be P2>P1
     float P2;
     printf("Enter value for pressure at canal end: |P2|\n");
-    if (scanf("%f" ,&P2)!=1){
+    if (scanf("%f",&P2)!=1){
         fprintf(stderr,"Error: P2 only accepts float value\n");
         return 1;
     }
@@ -42,5 +63,4 @@ int main () {
     float pressure_grad = deltaP / L;
     
     return 0;
-    
 }
