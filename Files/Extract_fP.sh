ac18522ddd19334b3c07b0bb8416da9dcef359f6
Pedro-Soto <118580776+Pedro-Soto@users.noreply.github.com>
2024-09-21 19:17:59 +0000

    Delete Files directory

--- Files/Extract_fP.sh
+++
@@ -1,9 +0,0 @@
-grep "fP_amplitude 0" input > Amplitude.dat
-grep "fP_frequency 0" input > Frequency.dat
-
-sed -i -e "s/fP_amplitude/ /g" Amplitude.dat
-sed -i -e "s/_/ /g" Amplitude.dat
-sed -i -e "s/fP_frequency/ /g" Frequency.dat
-
-
-
