dd70d08dd9160c0344d07e57778dd7829e0732fa
PSR-Off <sotodelrosariop@gmail.com>
2024-11-21 17:05:59 -0600

    Create numerical solution of resistance

---
+++ Files/resistance_num.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int zmax, a, b;
+    float mu1, mu2;
+    zmax=LONGUEUR;
+    a=J;
+    b=I;
+    Deltaz=0.01;
+    mu1=VISC1;
+    mu2=VISC2;
+
+
+
+
+}
