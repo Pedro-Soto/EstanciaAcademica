53cba8762e6f07a1eb44119c5245830fc4c62df9
PSR-Off <sotodelrosariop@gmail.com>
2024-11-26 17:09:41 -0600

    Changd plotter from cos to sin, to represent more accurately the canal

--- Files/plotter.sh
+++ Files/plotter.sh
@@ -30,9 +30,9 @@ gnuplot -persist <<- EOF
         print "Processing ".file
         plot file u 3:((\$4>0)?\$2:NaN), \
              file u 3:((\$4<0)?\$2:NaN), \
-             max(0, min($j_centre + ($b / 2) + $a * cos(2 * $PI * x / $zmax), $zmax)) \
+             max(0, min($j_centre + ($b / 2) + $a * sin(2 * $PI * x / $zmax), $zmax)) \
              title 'Wall Right' lc rgb "red" lw 3, \
-             max(0, min($j_centre - ($b / 2) - $a * cos(2 * $PI * x / $zmax), $zmax)) \
+             max(0, min($j_centre - ($b / 2) - $a * sin(2 * $PI * x / $zmax), $zmax)) \
              title 'Wall Left' lc rgb "red" lw 3
     }
     do for [i=1:num_files]{
